7bee427287d0a5ac3061761c342c488c3f7ba44e
christopher639 <bundichristopher639@gmail.com>
2024-07-27 00:09:42 +0300

    Seconsd commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import Header from "./Hearder.jsx"
 import Footer from "./Footer.jsx"
 import Food from "./assets/Food.jsx";
 import Card from "./assets/Card.jsx";
-import Button from "./assets/Button/Button.jsx";
+import Button from "./assets/Button.jsx";
+import Student from "./assets/student.jsx";
 function App() {
 
 
   return (
     <>
-    <Button/>
+    <Student name ='Spongebob' age ={10} isStudent ={true}/>
+    <Student name ='Christopher' age ={22} isStudent ={true}/>
+
+    <Student name ='Spongebob' age ={50} isStudent ={false}/>
     </>
   );
+}
+Student.prototype = {
+  
 }
 
 export default App
---
+++ src/assets/Button.jsx
@@ -0,0 +1,18 @@
+
+function Button(){
+    const styles ={
+            backgroundColor: " hsl(200,100% ,50%)",
+            color: "white",
+            padding: "10px 10px",
+            border: "none",
+            cursor: "pointer",
+            borderRadius: "5px",  
+    }
+    return(
+
+        <button style={styles}>Click Me !</button>
+
+    );
+}
+
+export default Button
